dced58ddb5558eb4fbbe3393f3becca3baeb4a64
Nurzhanat Nurymkyzy <57938202+nurzhanat20001@users.noreply.github.com>
2019-11-23 17:33:58 +0600

    Add files via upload

---
+++ task138.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,b,i;
+    cin>>b;
+    for(n=1;n<b;n++){
+        for(i=2;i<n;i++){
+            if(n%i==0){
+                break;
+            }
+        }
+        if(i==n)
+            cout<<n<<" ";
+        
+        }
+}
